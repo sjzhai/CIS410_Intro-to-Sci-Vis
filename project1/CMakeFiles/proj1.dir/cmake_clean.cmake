0daed85b5d255156d9c22229c6871eb3c3dd020c
ShengjieZhai <jacky122800@hotmail.com>
2018-02-01 16:55:12 -0800

    project1

---
+++ project1/CMakeFiles/proj1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj1.dir/proj1.cxx.o"
+  "proj1.pdb"
+  "proj1.app/Contents/MacOS/proj1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
